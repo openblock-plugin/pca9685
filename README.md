708164c3c0e93e712ff42379b18569245a4082d4
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:21:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# PCA9685 Module
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/pca9685?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-other-lightgrey.svg)
+
+The 16 channel PWM driver module based on PCA9685, uses IIC bus for communication.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,87 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'pca9685.name': 'PCA9685 Module',
+            'pca9685.description': 'The 16 channel PWM driver module based on PCA9685, uses IIC bus for communication.'
+        },
+        'ru': {
+            'pca9685.name': 'PCA9685 Module',
+            'pca9685.description': 'The 16 channel PWM driver module based on PCA9685, uses IIC bus for communication.'
+        },
+        'zh-cn': {
+            'pca9685.name': 'PCA9685 Module',
+            'pca9685.description': 'The 16 channel PWM driver module based on PCA9685, uses IIC bus for communication.'
+        },
+        'zh-tw': {
+            'pca9685.name': 'PCA9685 Module',
+            'pca9685.description': 'The 16 channel PWM driver module based on PCA9685, uses IIC bus for communication.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'PCA9685_CATEGORY': 'PCA9685',
+            'PCA9685_INIT': 'init pca9685 iic address %1',
+            'PCA9685_SETTOSERVOMODE': 'pca9685 set to servo mode',
+            'PCA9685_SETSERVOANGLE': 'pca9685 set channel %1 servo angle %2',
+            'PCA9685_SETALLSERVOANGLE': 'pca9685 set all servo angle %1',
+            'PCA9685_SETPWMFREQUENCY': 'pca9685 set pwm frequency %1',
+            'PCA9685_SETCHANNELPWM': 'pca9685 set channel %1 pwm value %2',
+            'PCA9685_SETALLCHANNELPWM': 'pca9685 set all channel pwm value %1'
+        },
+        'ru': {
+            'PCA9685_CATEGORY': 'PCA9685',
+            'PCA9685_INIT': 'init pca9685 iic address %1',
+            'PCA9685_SETTOSERVOMODE': 'pca9685 set to servo mode',
+            'PCA9685_SETSERVOANGLE': 'pca9685 set channel %1 servo angle %2',
+            'PCA9685_SETALLSERVOANGLE': 'pca9685 set all servo angle %1',
+            'PCA9685_SETPWMFREQUENCY': 'pca9685 set pwm frequency %1',
+            'PCA9685_SETCHANNELPWM': 'pca9685 set channel %1 pwm value %2',
+            'PCA9685_SETALLCHANNELPWM': 'pca9685 set all channel pwm value %1'
+        },
+        'zh-cn': {
+            'PCA9685_CATEGORY': 'PCA9685',
+            'PCA9685_INIT': 'init pca9685 iic address %1',
+            'PCA9685_SETTOSERVOMODE': 'pca9685 set to servo mode',
+            'PCA9685_SETSERVOANGLE': 'pca9685 set channel %1 servo angle %2',
+            'PCA9685_SETALLSERVOANGLE': 'pca9685 set all servo angle %1',
+            'PCA9685_SETPWMFREQUENCY': 'pca9685 set pwm frequency %1',
+            'PCA9685_SETCHANNELPWM': 'pca9685 set channel %1 pwm value %2',
+            'PCA9685_SETALLCHANNELPWM': 'pca9685 set all channel pwm value %1'
+        },
+        'zh-tw': {
+            'PCA9685_CATEGORY': 'PCA9685',
+            'PCA9685_INIT': 'init pca9685 iic address %1',
+            'PCA9685_SETTOSERVOMODE': 'pca9685 set to servo mode',
+            'PCA9685_SETSERVOANGLE': 'pca9685 set channel %1 servo angle %2',
+            'PCA9685_SETALLSERVOANGLE': 'pca9685 set all servo angle %1',
+            'PCA9685_SETPWMFREQUENCY': 'pca9685 set pwm frequency %1',
+            'PCA9685_SETCHANNELPWM': 'pca9685 set channel %1 pwm value %2',
+            'PCA9685_SETALLCHANNELPWM': 'pca9685 set all channel pwm value %1'
+        }
+    }
+};
